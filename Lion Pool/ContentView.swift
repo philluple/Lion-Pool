5fd109763e7d1d6f33b8c34c78680e54cf88dc0f
philluple <pnl2111@columbia.edu>
2022-06-16 12:11:17 -0700

    Initial Commit

---
+++ Lion Pool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lion Pool
+//
+//  Created by Phillip Le on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lion Pool/Lion_PoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lion_PoolApp.swift
+//  Lion Pool
+//
+//  Created by Phillip Le on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Lion_PoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
